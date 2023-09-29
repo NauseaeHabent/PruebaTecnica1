cca666c690aae631aac0f48f20fb047d0a4c329b
NauseaeHabent <jaroldgold@gmail.com>
2023-09-29 09:25:25 -0600

    Defined try catch for id duplicates in characters table

--- app/Http/Controllers/residentesController.php
+++ app/Http/Controllers/residentesController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Contracts\CharacterServiceInterface;
@@ -27,15 +27,23 @@ class residentesController extends Controller
         
     }
     public function store(Request $request) {
+        try{
         $id = $request->input('id');
         $name = $request->input('name');
         $status = $request->input('status');
         $species = $request->input('species');
 
         $this->characterService->saveCharacter($id, $name, $status, $species);
-
         return redirect()->back();
     }
+    catch (QueryException $e) {
+        if ($e->errorInfo[1] == 1062) { //error de llave primaria duplicada 
+            return redirect()->back()->with('error', 'Duplicado, este personaje ya existe en la DB');
+        } else { //error desconocido
+            return redirect()->back()->with('error', 'Un error sucedio en la DB');
+        }
+        
+    }}
 
     public function show() {
         $savedCharacters = Characters::all(); 
